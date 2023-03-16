95c92c9b09dd057d47df12184dbca9b01e9bf4b3
mitsukiraikiri <mitsukiraikiri@gmail.com>
2023-03-17 07:31:30 +0800

    sdfjh

---
+++ command1.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands\Memek;
+
+use Illuminate\Console\Command;
+
+class command1 extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        echo 'test';
+    }
+}
